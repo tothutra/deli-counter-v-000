32920300fb90ebeb646c5eb4927d49124085f2e3
Tra To <tothutra@gmail.com>
2018-08-12 04:56:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@ def line(deli)
   if deli.size == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently:"
-    deli.each_with_index {|person, idx| "#{idx + 1}. #{person}"}
+    current_line = "The line is currently:"
+    deli.each_with_index {|person, idx| current_line << " #{idx + 1}. #{person}"}
   end
 end
 
